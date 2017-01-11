9aedd279ba8389781f6a4312b0578515034ce174
tlong314 <tlong314@gmail.com>
2017-01-10 20:58:12 -0500

    Add files via upload
    added support for input elements

--- hoverSum.js
+++ hoverSum.js
@@ -1,3 +1,18 @@
+// This tool is built so you can add numbers you hover over, without repeating them.
+//
+// Methods:
+// hs.start();
+// hs.reset();
+// hs.destroy();
+// hs.pause(); // Let's you start checking sums with another hoverSum object
+// hs.resume(); // Let's control return to this hoverSum object. You probably have to pause all the other ones first...
+
+// Have an option to upload a word doc onto the page,
+// excel file, whatever.
+// Separate them to get this to work. For instance, seperate each word in the word doc into it's own div.
+// str.replace(/(\w) (\w)/g, "<span>$1</span>&nbsp;<span>$2</span>");
+// or just str.replace(/\s+/g, "</span> <span>"); Then str = "<span>" + str + "</span>";
+
 /**
  * @file hoverSum.js
  * @description A tool for adding numbers on a webpage together, simply by hovering over them.
@@ -36,6 +51,7 @@
 	
 	span.id = "hoverSumSpan";
 	span.style = "position: absolute; height: 15px; font: 12px Times New Roman; z-index: 999; top: 0; left: 0; border: 1px solid blue; color: #333; background: white; padding: 4px; border-radius: 4px;";
+	span.draggable = "true";
 	span.innerHTML = formatToCurrency(runningSum, true);
 	span.onclick = function() {
 		runningSum = 0;
@@ -54,10 +70,14 @@
 		window.addEventListener("mousemove", function(e) {
 			var t = e.target,
 				text = t.innerText.replace(/,|\$/g, ""),
-				num = parseFloat(text);
-				
+				num = parseFloat(text, 10);
+
 			if(isNaN(num)) {
-				return;
+				num = parseFloat(t.value, 10);
+
+				if(isNaN(num)) {
+					return;
+				}
 			}
 
 			if(/[^0-9]/.test(text)) { // For instance, dates like 6/22/14
@@ -67,11 +87,19 @@
 			if(!t.classList.contains("hoverCounted")) {
 				runningSum += num;
 				t.classList.add("hoverCounted");
-				span.style.left = e.clientX + 30 + "px";
+				span.style.left = document.body.scrollLeft + e.clientX + 30 + "px";
 				span.style.top = document.body.scrollTop + e.clientY - 40 + "px";
 				span.innerHTML = formatToCurrency(runningSum, true);
 				console.log(runningSum, e.clientX, e.clientY);
 			}
+		}, false);
+
+		window.addEventListener("drop", function(e) {
+			
+			console.log("mouseup", e.clientX, e.clientY);
+			
+			span.style.left = document.body.scrollLeft + e.clientX + 30 + "px";
+			span.style.top = document.body.scrollTop + e.clientY - 40 + "px";
 		}, false);
 	} // End hoverSum()
 	
